849add7cba3bfe167a23c9a736f19d52a2ecfa01
pengji <jipeng92@gmail.com>
2015-07-22 19:54:19 +0800

    #2 删除多余文件

--- 6/6_55.cpp
+++
@@ -1,9 +0,0 @@
-int DescNum(Bitree T){
-	//求树结点T 的子孙总数填入DescNum 域中,并返回该数
-	if(!T) 
-		return -1;
-	else 
-		d=(DescNum(T->lchild)+DescNum(T->rchild)+2); 
-	T->DescNum=d;
-	return d;
-}
--- 6/6_59.cpp
+++
@@ -1,7 +0,0 @@
-void Print_CSTree(CSTree T){
-	//输出孩子兄弟链表表示的树T的各边
-	for(child=T->firstchild;child;child=child->nextsib){
-		printf("(%c,%c),",T->data,child->data);
-		Print_CSTree(child);
-	}
-}
