b11680388e65927e6f038a0eb322c6a9e4822cd0
pengji <jipeng92@gmail.com>
2016-03-04 20:31:25 +0800

    modified:   test.c

--- SQLite/sqlite_api/test.c
+++ SQLite/sqlite_api/test.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 
+#define RED                  "\e[0;31m"
+#define GREEN                "\e[0;32m"
+#define L_GREEN              "\e[1;32m"
+#define YELLOW               "\e[1;33m"
+#define NONE                 "\e[0m"
+
+#define SQLITE_ZERO_MALLOC
+
 static int callback(void *data, int argc, char **argv, char **azColName){
    int i;
    fprintf(stderr, "%s: ", (const char*)data);
@@ -19,15 +27,33 @@ int main(int argc, char* argv[])
 	int  rc; 
 	char *sql;
 	const char* data = "Callback function called";
+	sqlite3_int64 used;
+	sqlite3_int64 highwater;
+
+	/*calculate memory use*/
+	used =  sqlite3_memory_used();
+	highwater = sqlite3_memory_highwater(0);
+	printf(L_GREEN "not running\n");
+	printf( RED "memory used: %d\n",used);
+	printf( YELLOW "memory highwater: %d\n",highwater);
+	printf("\e[0m\n",  __LINE__);
 
 	/* Open database */
-	rc = sqlite3_open("test1.db", &db);
+	rc = sqlite3_open("test.db", &db);
 	if( rc ){
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		exit(0);
 	}else{
 		fprintf(stdout, "Opened database successfully\n");
 	}
+
+	/* calculate memory use */
+	used =  sqlite3_memory_used();
+	highwater = sqlite3_memory_highwater(0);
+	printf(L_GREEN "open database\n");
+	printf(RED "memory used: %d\n",used);
+    printf( YELLOW "memory highwater: %d\n",highwater);
+	printf("\e[0m\n",  __LINE__);
 
 	/* Create SQL statement */
 	sql = "CREATE TABLE COMPANY("  \
@@ -45,6 +71,14 @@ int main(int argc, char* argv[])
 	}else{
 		fprintf(stdout, "Table created successfully\n");
 	}
+
+	/*calculate memory use*/
+    used =  sqlite3_memory_used();
+	highwater = sqlite3_memory_highwater(0);
+	printf(L_GREEN "create table\n");
+    printf( RED "memory used: %d\n",used);
+	printf( YELLOW "memory highwater: %d\n",highwater);
+    printf("\e[0m\n",  __LINE__);
 
 	/* Create SQL statement */
 	sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  \
@@ -65,6 +99,14 @@ int main(int argc, char* argv[])
 		fprintf(stdout, "Records created successfully\n");
 	}
 
+	/*calculate memory use*/
+    used =  sqlite3_memory_used();
+	highwater = sqlite3_memory_highwater(0);
+	printf(L_GREEN "insert operation\n");
+    printf( RED "memory used: %d\n",used);
+	printf( YELLOW "memory highwater: %d\n",highwater);
+    printf("\e[0m\n",  __LINE__);
+
 	/* Create SQL statement */
 	sql = "SELECT * from COMPANY";
 
@@ -76,6 +118,14 @@ int main(int argc, char* argv[])
 	}else{
 		fprintf(stdout, "Operation done successfully\n");
 	}
+
+	/*calculate memory use*/
+    used =  sqlite3_memory_used();
+	highwater = sqlite3_memory_highwater(0);
+	printf(L_GREEN "select operation\n");
+    printf( RED "memory used: %d\n",used);
+	printf( YELLOW "memory highwater: %d\n",highwater);
+    printf("\e[0m\n",  __LINE__);
 
 	/* Create merged SQL statement */
 	sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1; " \
@@ -90,6 +140,14 @@ int main(int argc, char* argv[])
 		fprintf(stdout, "Operation done successfully\n");
 	}
 
+	/*calculate memory use*/
+    used =  sqlite3_memory_used();
+	highwater = sqlite3_memory_highwater(0);
+	printf(L_GREEN "update operation\n");
+    printf( RED "memory used: %d\n",used);
+	printf( YELLOW "memory highwater: %d\n",highwater);
+    printf("\e[0m\n",  __LINE__);
+
 	/* Create merged SQL statement */
 	sql = "DELETE from COMPANY where ID=2; " \
          "SELECT * from COMPANY";
@@ -102,6 +160,14 @@ int main(int argc, char* argv[])
 	}else{
 		fprintf(stdout, "Operation done successfully\n");
 	}
+
+	/*calculate memory use*/
+    used =  sqlite3_memory_used();
+	highwater = sqlite3_memory_highwater(0);
+	printf(L_GREEN "delete operation\n");highwater = sqlite3_memory_highwater(0);
+    printf( RED "memory used: %d\n",used);
+	printf( YELLOW "memory highwater: %d\n",highwater);
+    printf("\e[0m\n",  __LINE__);
 
 	sqlite3_close(db);
 	return 0;  
