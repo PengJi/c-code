f13146de95a2b7a827e781dc07f0af078acba84f
pengji <jipeng92@gmail.com>
2017-04-19 22:03:03 +0800

    modified sizeof.cpp

--- cpp/offer/sizeof.cpp
+++ cpp/offer/sizeof.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 
 void print_sizeof(){
+	cout<<"sizeof(char): "<< sizeof(char)<<endl;
+	cout<<"sizeof(int): "<<sizeof(int)<<endl;
+	cout<<"sizeof(long): "<<sizeof(long)<<endl;
+}
+
+void print_struct(){
 	typedef union {
  	   long i;
-	    int k[5];
-	    char c;
+	   int k[5];
+	   char c;
 	} DATE;
 	struct data {
     	int cat;
@@ -15,11 +21,14 @@ void print_sizeof(){
 	} too;
 	DATE max;
 
-	printf("%d",sizeof(struct data)+sizeof(max));
-
+	cout<<"sizeof(DATE): "<<sizeof(DATE)<<endl;
+	cout<<"sizeof(data): "<< sizeof(data)<<endl;
+	cout<<"sizeof(max): "<< sizeof(max)<<endl;
+	cout<<"sizeof(struct data)+sizeof(max): "<<sizeof(struct data)+sizeof(max)<<endl;
 }
 
 int main(){
 	print_sizeof();
+	print_struct();
 	return 0;
 }
