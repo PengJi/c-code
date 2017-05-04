e234268e3375521980ef7254cada2faec4ec87e3
pengji <jipeng92@gmail.com>
2017-05-04 10:15:05 +0800

    modified gather.cpp

--- cpp/offer/gather.cpp
+++ cpp/offer/gather.cpp
@@ -1,3 +1,9 @@
+struct ListNode{
+	int val;
+	struct ListNode *next;
+	ListNode(int x):
+		val(x),next(NULL){}
+}
 class Solution{
 public:
 	//输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
@@ -22,6 +28,42 @@ public:
 		}
 		return exponent<0?1/r:r;
 	}
+
+	//输入一个链表，反转链表后，输出链表的所有元素。
+    ListNode* ReverseList(ListNode* pHead) {
+		/*
+        ListNode *pre = NULL;
+        ListNode *p = NULL;
+        pre = pHead;
+        p = pHead->next;
+        pre -> next = NULL;
+        while(p != NULL){
+        	pre = pHead;
+            pHead = p;
+            p = p->next;
+            pHead -> next = pre;
+        }
+        
+        return pHead;
+		*/
+		ListNode *pre = NULL;
+        ListNode *next = NULL;
+        while(pHead != NULL){
+            next = pHead -> next;
+            pHead -> next = pre;
+            pre = pHead;
+            pHead = next;
+        }
+        return pre;
+    }
+	
+	//输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
+	ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        
+    }
+
+	
 };
 
 
