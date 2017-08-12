66b0d4add93a860e17e77ed46bec45a5e8609db4
pengji <jipeng92@gmail.com>
2017-08-12 11:47:17 +0800

    rm C/test.c

--- C/test.c
+++ C/test.c
@@ -2,10 +2,24 @@
 #include <stdio.h>      /* printf */
 #include <math.h>       /* pow */
 
+void f(int *m){
+	*m=5;
+}
+
 int main ()
 {
   printf ("7 ^ 3 = %f\n", pow (7.0, 3.0) );
   printf ("4.73 ^ 12 = %f\n", pow (4.73, 12.0) );
   printf ("32.01 ^ 1.54 = %f\n", pow (32.01, 1.54) );
+
+  int n = 1;
+  int *k = &n;
+  printf("%d\n",n);
+  printf("%d\n",*k);
+  f(&n);
+  f(k);
+  printf("%d\n",n);
+  printf("%d\n",*k);
+
   return 0;
 }
