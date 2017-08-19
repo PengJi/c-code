01adf1b59cf2819cf467af192bc933746ac95d3c
pengji <jipeng92@gmail.com>
2017-08-19 20:24:00 +0800

    modified unique.ptr.cpp

--- prac/smart_pointer/unique_ptr.cpp
+++ prac/smart_pointer/unique_ptr.cpp
@@ -4,6 +4,18 @@
 
 using namespace std;
 
+/*
+ * unique_ptr可实现如下功能：
+ * 1. 为动态申请的内存提供异常安全
+ * 2. 将动态申请的内存所有权传递给某函数
+ * 3. 从某个函数返回动态申请内存的所有权
+ * 4. 在容器中保存指针
+ * 5. auto_ptr应该具有的功能
+ *
+ * 编译
+ * g++ unique_ptr.cpp -std=c++11
+ */
+
 class Test
 {
 public:
@@ -20,7 +32,7 @@ public:
 		return str;
 	}
 
-	void setStr()(string s){
+	void setStr(string s){
 		str = s;
 	}
 	void print(){
@@ -28,6 +40,41 @@ public:
 	}
 private:
 	string str;
+};
+
+unique_ptr<Test> fun(){
+	return unique_ptr<Test>(new Test("789"));
 }
 
+int main(){
+	unique_ptr<Test> ptest(new Test("123"));
+	unique_ptr<Test> ptest2(new Test("456"));
+	ptest->print();
+	ptest2 = std::move(ptest); //不能直接ptest2=ptest
+	if(ptest == NULL)
+		cout<<"ptest=NULL"<<endl;
 
+	Test *p = ptest2.release();
+	p->print();
+	ptest.reset(p);
+	ptest->print();
+	ptest2 = fun(); //可以用=，使用了移动构造函数
+	ptest2->print();
+
+	return 0;
+}
+
+/*
+ * 输出:
+ * Test create
+ * Test create
+ * 123
+ * Test delete: 456
+ * ptest=NULL
+ * 123
+ * 123
+ * Test create
+ * 789
+ * Test delete: 789
+ * Test delete: 123
+ */
