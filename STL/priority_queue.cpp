1de6921910fc28bc80930581e3549879a81698ee
pengji <jipeng92@gmail.com>
2017-08-26 09:36:16 +0800

    add stl priority_queue

---
+++ STL/priority_queue.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+/*
+ * priority_queue<Type, Container, Functional>
+ * Type 数据类型
+ * Container 保存数据的容器
+ * Functional 元素的比较方式
+ */
+
+struct Node{
+	int x,y;
+	Node(int a=0,int b=0):x(a),y(b){}
+};
+
+struct cmp{
+	bool operator()(Node a,Node b){
+		if(a.x == b.x)
+			return a.y>b.y;
+
+		return a.x>b.y;
+	}
+};
+
+int main(){
+	// 默认排序，大顶堆
+	priority_queue<int> q;
+	int a[]={3,5,9,6,2};
+	for(int i = 0;i < 5; i++)
+			q.push(a[i]);
+
+	for(int j = 0; j < 5; j++){
+		cout<<q.top()<<" ";
+		q.pop();
+	}
+
+	// 小顶堆
+	priority_queue<Node,vector<Node>,cmp> q2;
+	for(int i = 0;i<10;i++)
+		q2.push(Node(i+1,i+2));
+
+	while(!q2.empty()){
+		cout << q2.top().x << " " << q2.top().y << endl;
+		q2.pop();
+	}
+
+	return 0;
+}
