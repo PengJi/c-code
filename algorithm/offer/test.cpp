f38f1bfa2fb8bde383c9c1782eccefd06c9eab29
pengji <jipeng92@gmail.com>
2017-09-26 23:08:19 +0800

    modified test.cpp

--- algorithm/offer/test.cpp
+++ algorithm/offer/test.cpp
@@ -172,3 +172,216 @@ vector<vector<int> > FindContinuousSequence(int sum) {
 		cur += end;
 	}
 }
+
+void preorderTraversal(TreeNode *root) {
+	stack<TreeNode*> stk;
+	TreeNode *node = root;
+	while(node != NULL || !stk.empty()){
+		while(node != NULL){
+			printf("%d ",node->val);
+			stk.push(node);
+			node = node->left;
+		}
+
+		if(!stk.empty()){
+			node = stk.pop();
+			node = node->right;
+		}
+	} 
+}
+
+void middleorderTraversal(TreeNode *root) {
+	stack<int> stk;
+	TreeNode *node = root;
+	while(node != NULL || stk.empty()){
+		while(node != NULL){
+			stk.push(node);
+			node = node->left;
+		}
+
+		if(!stk.empty()){
+			node = stk.pop();
+			printf("%d ",node->val);
+			node = node -> right;
+		}
+	}
+}
+
+void postorderTraversal(TreeNode *root) {
+	stack<int> stk;
+	TreeNode node = root;
+	TreeNode lastVisit = root;
+	while(node != NULL || !stk.empty()){
+		while(node != NULL){
+			stk.push(node);
+			node = node->left;
+		}
+
+		node = stk.top();
+		if(node->right == NULL || node->right == lastVisit){
+			printf("%d ",node->val);
+			stk.pop();
+			node = NULL;
+			lastVisit = node;
+		}else{
+			node = node->right;
+		}
+	}
+}
+
+int count = 0;
+TreeNode* KthNode(TreeNode* pRoot, int k){
+	if(pRoot){
+		TreeNode *t = KthNode(pRoot->left,k);
+		if(t) return t;
+		if(++count == k) return pRoot;
+		t = KthNode(pRoot->right,k);
+		if(t) return t;
+	}
+	return NULL;
+}
+
+int partition(int a[], int low, int high){
+	int pivot = a[log];
+	while(low < high){
+		while(low < high && a[high] >= pivot) --high;
+		swap(&a[low],&a[high]);
+		while(low < high && a[low] <= pivot) ++low;
+		swap(&a[low],&a[high]);
+	}
+	return low;
+}
+void quickSort(int a[],int low,int high){
+	if(low < high){
+		int pivot = partition(a,low,high);
+		quickSort(a,low,pivot-1);
+		quickSort(a,pivot+1,high);
+	}
+}
+
+void heapAdjust(int H[],int s,int lenght){
+	int tmp = H[s];
+	int child = 2*s+1;
+	while(child < length){
+		if(child+1 < length && H[child] < H[child+1])
+			child++;
+
+		if(H[s] < H[child]){
+			H[s] = H[child];
+			s = child;
+			child = 2*s+1;
+		}else{
+			break;
+		}
+
+		H[s] = tmp;
+	}
+}
+vodi BuildHeap(int H[],int length){
+	for(int i = (length-1)/2;i>=0;i--){
+		heapAdjust(H,i,length);
+	}
+}
+vodi HeapAdjust(int H[],int length){
+	BuildHeap(H,length);
+	for(int i = length-1;i>0;i--){
+		int temp = H[i];
+		H[i] = H[0];
+		H[0] = temp;
+		HeapAdjust(H,0,i);
+	}
+}
+
+vector<vector<int>> allRes;
+vector<int> tmp;
+vodi dfsFind(TreeNode *node,int left){
+	tmp.push_back(node->val);
+	if(left-node->val == 0 && !node->left && !node->right)
+		allRes.push_back(tmp);
+	else{
+		if(node->left)
+			dfsFind(node->left,left-node->val);
+		if(node->right)
+			dfsFind(node->right,left-node->val);
+	}
+	tmp.pop_back();
+}
+vector<vector<int>> FindPath(TreeNode *root,int expectNumber){
+	if(root)
+		dfsFind(root,expectNumber);
+	return allRes;
+}
+
+vecotr<int> PrintFromtoBottom(TreeNode *rt){
+	queue<TreeNode* > q;
+	q.push(rt);
+	vector<int> r;
+	while(!q.empty()){
+		rt = q.front();
+		q.pop();
+		r.push_back(rt->val);
+		q.push(rt->left);
+		q.push(rt->right);
+	}
+}
+
+vector<vector<int>> Print(TreeNode *root){
+	if(root == NULL)
+		return vec;
+	vector<vector<int>> vec;
+
+	queue<TreeNode *> q;
+	q.push(root);
+
+	while(!q.empty()){
+		int i = 0;
+		int size = q.size();
+		for(i;i<szie;i++){
+			TreeNode *t = q.front();
+			v.push_back(t->val);
+			q.pop();
+			if(t->left)
+				q.push(t->left);
+			if(t->right)
+				q.push(t->right);
+		}
+	}
+}
+
+vector<vector<int>> Print(TreeNode *root){
+	vector<vector<int>> res;
+	if(root == NULL)
+		return res;
+	stack<TreeNode*> stk1;
+	stack<TreeNode *> stk2;
+
+	bool mode = true;
+	while(!stk1.empty()){
+		stk2 = stk1;
+		stk1.clear();
+		vector<int> row;
+
+		while(!stk2.empty()){
+			TreeNode *tmp = stk2.top();
+			stk2.pop();
+			row.push_back(tmp->val);
+			if(model == true){
+				if(tmp->left)
+					stk1.push(tmp->left);
+				if(tmp->right)
+					stk2.push(tmp->right);
+			}else{
+				if(tmp->right)
+					stk1.push(tmp->right);
+				if(tmp->left)
+					stk1.push(tmp->left);
+			}
+		}
+		model = !model;
+		res.push_back(row);
+	}
+
+	return res;
+}
+
+
