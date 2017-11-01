d9302ac7ab6feedb79b528767b225f10721e6e6a
pengji <jipeng92@gmail.com>
2017-11-01 21:14:51 +0800

    modified list.cpp 2

--- algorithm/leetcode/list.cpp
+++ algorithm/leetcode/list.cpp
@@ -9,6 +9,30 @@
 class Solution {
 public:
 	/*
+	 * 2. 
+	 * Add Two Numbers
+	 * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+	 */
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+	    ListNode preHead(0), *p = &preHead;
+	    int extra = 0;
+	    while (l1 || l2 || extra) {
+	        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + extra;
+	        extra = sum / 10;
+	        p->next = new ListNode(sum % 10);
+	        p = p->next;
+	        l1 = l1 ? l1->next : l1;
+	        l2 = l2 ? l2->next : l2;
+	    }
+	    return preHead.next;
+	}
+
+	/*
 	 * 143
 	 * Reorder List
 	 * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
