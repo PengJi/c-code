2ae848d9d5ca0b06fcd6356f180e3e499dcbd8c8
pengji <jipeng92@gmail.com>
2017-11-07 23:28:19 +0800

    modified string 5

--- algorithm/leetcode/string.cpp
+++ algorithm/leetcode/string.cpp
@@ -63,4 +63,24 @@ When we found a palindrome, check if it's the longest one. Time complexity O(n^2
         
         return res;
     }
+
+    string longestPalindrome(string s) {
+        if(s.size()<=1) return s; 
+        int start = 0, maxLen = 1, n = s.size();
+        bool isPal[1000][1000] = {false};
+        
+        for(int i=n-1; i>=0; i--) {
+            for(int j=i; j<n; j++) {
+                if((i+1>j-1 || isPal[i+1][j-1]) && s[i]==s[j]) {
+                    isPal[i][j] = true;
+                    if(j-i+1>maxLen) {
+                        maxLen = j-i+1;
+                        start = i;
+                    }
+                }
+            }
+        }
+        
+        return s.substr(start,maxLen);
+    }
 };
