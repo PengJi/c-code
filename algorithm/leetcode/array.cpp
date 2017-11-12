e478d2a11e767590c469a264b9ea8124622002ab
pengji <jipeng92@gmail.com>
2017-11-12 20:27:17 +0800

    modified array 39

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -78,6 +78,51 @@ The median is (2 + 3)/2 = 2.5
         return -1;
     }
 
+    /*
+     * 39. 
+     * Combination Sum
+     * Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+
+solution:
+http://bangbingsyb.blogspot.ca/2014/11/leetcode-combination-sum-i-ii.html
+     */
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        vector<vector<int> > allSol;
+        vector<int> sol;
+        if(candidates.empty()) return allSol;
+        sort(candidates.begin(),candidates.end());
+        findCombSum(candidates, 0, target, sol, allSol);
+        return allSol;
+    }
+    
+    void findCombSum(vector<int> &candidates, int start, int target, vector<int> &sol, vector<vector<int>> &allSol) {
+        if(target==0) {
+            allSol.push_back(sol);
+            return;
+        }
+        
+        for(int i=start; i<candidates.size(); i++) {
+            if(i>start && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]<=target) {
+                sol.push_back(candidates[i]);
+                findCombSum(candidates, i, target-candidates[i], sol, allSol);
+                sol.pop_back();
+            }
+        }
+    }
+
 	/*
 	 * 287. 
 	 * Find the Duplicate Number
