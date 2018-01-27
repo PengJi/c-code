4a874ee3796ea0faa507a2dbcf036bf618e7384a
pengji <jipeng92@gmail.com>
2018-01-27 11:20:59 +0800

    add Pthreads

--- tools/OpenMP/hello.c
+++ tools/OpenMP/hello.c
@@ -5,17 +5,28 @@ int main()
 {  
 	int i;
 #pragma omp task
-	printf("task\n");
+	{
+		printf("task\n");
+		printf("Thread ID: %d\n",omp_get_thread_num());
+	}
 
 #pragma omp task
-	printf("task\n");
+	{
+		printf("task\n");
+		printf("Thread ID: %d\n",omp_get_thread_num());
+	}
 
 #pragma omp parallel
 	printf("parallel\n");
 
+#pragma omp parallel num_threads(2)
+	{
+		printf("num_threads\n");
+		printf("Thread ID: %d\n",omp_get_thread_num());
+	}
+
 	return 0;
 }
-
 /*
  * 编译
  * gcc -fopenmp hello.c
