a33cf1568a10bbde41b5a757576371bc70be7153
pengji <jipeng92@gmail.com>
2018-01-27 09:19:13 +0800

    modify hello.c

--- tools/OpenMP/hello.c
+++ tools/OpenMP/hello.c
@@ -3,10 +3,22 @@
   
 int main()  
 {  
-#pragma omp parallel  
-    {  
-		printf("test\n");
-    }  
-	
+	int i;
+#pragma omp task
+	printf("task\n");
+
+#pragma omp task
+	printf("task\n");
+
+#pragma omp parallel
+	printf("parallel\n");
+
 	return 0;
 }
+
+/*
+ * 编译
+ * gcc -fopenmp hello.c
+ * ./a.out
+ *
+ */
