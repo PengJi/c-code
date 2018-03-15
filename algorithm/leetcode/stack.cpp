37297f553a6637409bdf63db41da89b703b9f7f0
pengji <jipeng92@gmail.com>
2018-03-15 21:57:46 +0800

    add 20

--- algorithm/leetcode/stack.cpp
+++ algorithm/leetcode/stack.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    /*
+    20. Valid Parentheses
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+     */
+    bool isValid(string s) {
+        string left = "([{";
+        string right = ")]}";
+        stack<char> stk;
+
+        for(auto c : s){
+            if(left.find(c) != string::npos){
+                stk.push(c);
+            }else{
+                if(stk.empty() || stk.top() != left[right.find(c)])
+                    return false;
+                else
+                    stk.pop();
+            }
+        }
+
+        return stk.empty();
+    }
+
 	/*
 	 * 150
 	 * Evaluate Reverse Polish Notation
