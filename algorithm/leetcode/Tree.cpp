43b319e72a391f94a947263c78d50b8f0fe64be7
pengji <jipeng92@gmail.com>
2018-03-18 22:28:30 +0800

    add 96

--- algorithm/leetcode/Tree.cpp
+++ algorithm/leetcode/Tree.cpp
@@ -47,6 +47,32 @@ http://bangbingsyb.blogspot.ca/2014/11/leetcode-binary-tree-inorder-traversal.ht
         return allNodeValues;
     }
 
+  /**
+   * 96. Unique Binary Search Trees
+   * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+   */
+  int numTrees(int n) {
+      vector<int> f(n+1,0);
+
+      f[0] = 1;
+      f[1] = 1;
+      for(int i=2; i<=n; ++i){
+        for(int k=1; k<=i; ++k)
+          f[i] += f[k-1] * f[i-k];
+      }
+
+      return f[n];
+  }
+
 	/*
 	 * 101. Symmetric Tree
 	 * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
