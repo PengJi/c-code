16829298b7eb58db445a37976747936e40e700e8
pengji <jipeng92@gmail.com>
2018-03-18 22:55:52 +0800

    add 95

--- algorithm/leetcode/Tree.cpp
+++ algorithm/leetcode/Tree.cpp
@@ -48,6 +48,57 @@ http://bangbingsyb.blogspot.ca/2014/11/leetcode-binary-tree-inorder-traversal.ht
     }
 
   /**
+   * 95. Unique Binary Search Trees II
+   * Given an integer n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+
+For example,
+Given n = 3, your program should return all 5 unique BST's shown below.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+   */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+public:
+  vector<TreeNode*> generateTrees(int n) {
+    if(n == 0)
+      return generate(1,0);
+    return generate(1,n);
+  }
+private:
+  vector<TreeNode *> generate(int start, int end){
+    vector<TreeNode*> subTree;
+    if(start > end){
+      subTree.push_back(nullptr);
+      return subTree;
+    }
+    for(int k=start; k <= end; ++k){
+      vector<TreeNode*> leftSubs = generate(start, k-1);
+      vector<TreeNode*> rightSubs = generate(k+1,end);
+      for(auto i : leftSubs){
+        for(auto j : rightSubs){
+          TreeNode *node = new TreeNode(k);
+          node->left = i;
+          node->right = j;
+          subTree.push_back(node);
+        }
+      }
+    }
+
+    return subTree;
+  }
+
+  /**
    * 96. Unique Binary Search Trees
    * Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
 
