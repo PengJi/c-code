d6e281f4ce87c63044cbbac5d8a12b9db61541fb
pengji <jipeng92@gmail.com>
2018-03-18 22:28:10 +0800

    add 92

--- algorithm/leetcode/list.cpp
+++ algorithm/leetcode/list.cpp
@@ -64,6 +64,39 @@ Output: 7 -> 0 -> 8
         return l3;
     }
 
+    /**
+     * 92. Reverse Linked List II
+     * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+     *
+     * For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+     */
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if(m == n)
+            return head;
+        n -= m;
+        ListNode prehead(0);
+        prehead.next = head;
+        ListNode *pre = &prehead;
+        while(--m)
+            pre = pre->next;
+        ListNode *pstart = pre->next;
+        while(n--){
+            ListNode *p = pstart->next;
+            pstart->next = p->next;
+            p->next = pre->next;
+            pre->next = p;
+        }
+
+        return prehead.next;
+    }
+
 	/*
 	 * 143
 	 * Reorder List
