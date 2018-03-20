f46b84f9db7abe4e6356022c744fd90234365d89
pengji <jipeng92@gmail.com>
2018-03-20 19:14:23 +0800

    add 35

--- algorithm/leetcode/find.cpp
+++ algorithm/leetcode/find.cpp
@@ -33,4 +33,45 @@ return [3, 4].
 	    }
 	    return l;
 	}
+
+	/**
+	 * 35. Search Insert Position
+	 * Given a sorted array and a target value, return the index if the target is found. If not, 
+	 * return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+Input: [1,3,5,6], 2
+Output: 1
+
+Example 3:
+Input: [1,3,5,6], 7
+Output: 4
+
+Example 4:
+Input: [1,3,5,6], 0
+Output: 0
+	 */
+	//重新实现 lower_bound
+	int searchInsert(vector<int>& nums, int target) {
+        return distance(nums.begin(),lower_bound(nums.begin(),nums.end(),target));
+    }
+    template<typename ForwardIterator, typename T>
+    ForwardIterator lower_bound(ForwardIterator first,ForwardIterator last, T value){
+    	while(first != last){
+    		auto mid = next(first, distance(first, last)/2);
+
+    		if(value > *mid)
+    			first = ++mid;
+    		else 
+    			last = mid;
+    	}
+
+    	return first;
+    }
 };
