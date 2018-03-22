012d62842b4d844040cce3a0c93a05aacf4ad820
pengji <jipeng92@gmail.com>
2018-03-22 16:38:49 +0800

    add 119

--- algorithm/leetcode/means.cpp
+++ algorithm/leetcode/means.cpp
@@ -80,8 +80,16 @@ Return [1,3,3,1].
 Note:
 Could you optimize your algorithm to use only O(k) extra space?
      */
+    //滚动数组
     vector<int> getRow(int rowIndex) {
-        
+        vector<int> array;
+        for(int i=0;i<=rowIndex;i++){
+        	for(int j=i-1;j>0;j--)
+        		array[j] = array[j-1] + array[j];
+        	array.push_back(1);
+        }
+
+        return array;
     }
-    
+
 };
