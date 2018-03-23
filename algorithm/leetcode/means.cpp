3e46e2e3b984666947f12f2463b4e20f1c1e8745
pengji <jipeng92@gmail.com>
2018-03-23 21:04:03 +0800

    add 78

--- algorithm/leetcode/means.cpp
+++ algorithm/leetcode/means.cpp
@@ -41,6 +41,38 @@ For the purpose of this problem, assume that your function returns 0 when the re
 	}
 
 	/**
+	 * 78. Subsets
+	 * Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+	 */
+	vector<vector<int>> subsets(vector<int>& nums) {
+        sort (nums.begin(), nums.end());
+        int elem_num = nums.size();
+        int subset_num = pow (2, elem_num);
+        vector<vector<int> > subset_set (subset_num, vector<int>());
+        for (int i = 0; i < elem_num; i++)
+            for (int j = 0; j < subset_num; j++)
+                if ((j >> i) & 1)
+                    subset_set[j].push_back (nums[i]);
+        return subset_set;
+    }
+
+	/**
 	 * 50. Pow(x, n)
 	 * Implement pow(x, n).
 
