7522f0f29fd1e79325a639be82679ccbfa94b2ef
pengji <jipeng92@gmail.com>
2018-03-25 20:12:57 +0800

    add 98

--- algorithm/leetcode/Tree.cpp
+++ algorithm/leetcode/Tree.cpp
@@ -124,6 +124,38 @@ Given n = 3, there are a total of 5 unique BST's.
       return f[n];
   }
 
+  /**
+   * 98. Validate Binary Search Tree
+   * 
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+    2
+   / \
+  1   3
+Binary tree [2,1,3], return true.
+Example 2:
+    1
+   / \
+  2   3
+Binary tree [1,2,3], return false.
+   */
+  bool isValidBST(TreeNode* root) {
+    return isValidBST(root,NULL,NULL);
+  }
+  bool isValidBST(TreeNode *root,TreeNode* lower, TreeNode* upper){
+    if(root == nullptr)
+      return true;
+    if(lower && root->val <= lower->val || upper && root->val >= upper->val)
+    return isValidBST(root->left,lower,root)
+      && isValidBST(root->right,root,upper);
+  }
+
 	/*
 	 * 101. Symmetric Tree
 	 * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
