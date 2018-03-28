4d309856401566f9a317a072a0c257a853382b3f
pengji <jipeng92@gmail.com>
2018-03-28 18:18:06 +0800

    add 129

--- algorithm/leetcode/Tree.cpp
+++ algorithm/leetcode/Tree.cpp
@@ -448,6 +448,38 @@ Return 6.
     max_sum = max(max_sum,sum);
     return max(r,l) >0 ? max(r,l)+root->val:root->val;
   }
+
+  /**
+   * 129. Sum Root to Leaf Numbers
+   * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+For example,
+
+    1
+   / \
+  2   3
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+
+Return the sum = 12 + 13 = 25.
+   */
+  int sumNumbers(TreeNode* root) {
+    return dfs(root,0);
+  }
+  int dfs(TreeNode *root, int sum){
+    if(root == nullptr)
+      return 0;
+
+    if(root->left == nullptr && root->right == nullptr)
+      return sum*10 + root->val;
+
+    return dfs(root->left, sum*10+root->val) + dfs(root->right, sum*10+root->val);
+  }
+
 	/*
 	 * 145
 	 * Binary Tree Postorder Traversal
