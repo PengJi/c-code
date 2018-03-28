6d75d9cad6d2dba61a9693e2e36f65ba5e18ead6
pengji <jipeng92@gmail.com>
2018-03-28 18:40:23 +0800

    add 102

--- algorithm/leetcode/Tree.cpp
+++ algorithm/leetcode/Tree.cpp
@@ -204,6 +204,42 @@ Bonus points if you could solve it both recursively and iteratively.
         return true;
   }
 
+  /**
+   * 102. Binary Tree Level Order Traversal
+   * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+   */
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> result;
+    traverse(root,1,result);
+    return result;
+  }
+  void traverse(TreeNode *root, size_t level, vector<vector<int>> &result){
+    if(!root)
+      return;
+
+    if(level > result.size())
+      result.push_back(vector<int>());
+
+    result[level-1].push_back(root->val);
+
+    traverse(root->left, level+1, result);
+    traverse(root->right, level+1, result);
+  }
+
     /**
      * 104. Maximum Depth of Binary Tree
      * Given a binary tree, find its maximum depth.
