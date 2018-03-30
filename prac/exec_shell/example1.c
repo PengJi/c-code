eabfc7bcfecbb3930e41386cb9834754aa863fce
pengji <jipeng92@gmail.com>
2018-03-30 11:14:03 +0800

    add exec_shell

---
+++ prac/exec_shell/example1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>    
+#include <string.h>
+#include <errno.h>
+int main(int argc,char*argv[]){    
+    FILE *fstream=NULL;      
+    char buff[1024];    
+    memset(buff,0,sizeof(buff));    
+    if(NULL==(fstream=popen("ifconfig","r")))      
+    {     
+        fprintf(stderr,"execute command failed: %s",strerror(errno));      
+        return -1;      
+    }     
+    while(NULL!=fgets(buff, sizeof(buff), fstream)) {  
+            printf("%s",buff);    
+    }  
+    pclose(fstream);    
+    return 0;     
+}
