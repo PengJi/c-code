2f4642518d39d9f3af002b63eb3a75731965dbb3
pengji <jipeng92@gmail.com>
2018-03-30 18:24:48 +0800

    add 100

--- algorithm/leetcode/Tree.cpp
+++ algorithm/leetcode/Tree.cpp
@@ -157,6 +157,80 @@ Binary tree [1,2,3], return false.
       && isValidBST(root->right,root,upper);
   }
 
+  /**
+   * 100. Same Tree
+   * 
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+   */
+  //递归版
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(!p && !q)
+      return true;
+    if(!p || !q)
+      return false;
+
+    return p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right, q->right);
+  }
+  //迭代版
+  bool isSameTree(TreeNode *p, TreeNode *q) {
+    stack<TreeNode *> s;
+    s.push(p);
+    s.push(q);
+
+    while(!s.empty()){
+      p = s.top();
+      s.pop();
+      q = s.top();
+      s.pop();
+
+      if(!p && !q)
+        continue;
+      if(!p || !q)
+        return false;
+      if(p->val != q->val)
+        return false;
+
+      s.push(p->left);
+      s.push(q->left);
+
+      s.push(p->right);
+      s.push(q->right);
+    }
+
+    return true;
+  }
+
 	/*
 	 * 101. Symmetric Tree
 	 * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
