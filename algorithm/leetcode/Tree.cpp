602d00d5f183a52cc1f334638019ec3c4dbc5593
pengji <jipeng92@gmail.com>
2018-03-31 18:06:51 +0800

    add 110

--- algorithm/leetcode/Tree.cpp
+++ algorithm/leetcode/Tree.cpp
@@ -572,6 +572,54 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
       return root;
     }
 
+    /**
+     * 110. Balanced Binary Tree
+     * Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example 1:
+
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+Example 2:
+
+Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+Return false.
+     */
+  bool isBalanced(TreeNode* root) {
+    return balancedHeight(root) >= 0;
+  }
+  int balancedHeight(TreeNode *root){
+    if(root == nullptr)
+      return 0;
+
+    int left = balancedHeight(root->left);
+    int right = balancedHeight(root->right);
+
+    if(left < 0 || right < 0 || abs(left-right) > 1) //剪枝
+      return -1;
+
+    return max(left,right) + 1; //三方合并
+  }
+
 	/*
 	 * 111
 	 * Minimum Depth of Binary Tree
