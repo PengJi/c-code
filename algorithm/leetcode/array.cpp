e23eeb6ae0313a428621af8aff47e7790abd5e9b
pengji <jipeng92@gmail.com>
2018-04-02 19:13:45 +0800

    add 26

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -116,6 +116,47 @@ For example, given n = 3, a solution set is:
     	}
     }
 
+    /**
+     * 26. Remove Duplicates from Sorted Array
+     * Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+It doesn't matter what you leave beyond the new length.
+     */
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty())
+            return 0;
+
+        int index = 0;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[index] != nums[i])
+                nums[++index] = nums[i];
+        }
+        return index+1;
+    }
+    //使用STL
+    int removeDuplicates(vector<int>& nums) {
+        return distance(nums.begin(), unique(nums.begin(),nums.end()));
+    }
+    //使用STL
+    int removeDuplicates(vector<int>& nums) {
+        return distance(nums.begin(),removeDuplicates(nums.begin(),nums.end(),nums.begin()));
+    }
+    template<typename InIt, typename OutIt>
+    OutIt removeDuplicates(InIt first, InIt last, OutIt output){
+        while(first != last){
+            *output++ = *first;
+            first = upper_bound(first,last, *first);
+        }
+        return output;
+    }
+
     /*
      * 39. 
      * Combination Sum
