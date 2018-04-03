6e0674d8f4ab9ed725db6f8ed72bce1e98230e31
pengji <jipeng92@gmail.com>
2018-04-03 19:10:11 +0800

    add 33

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -157,6 +157,41 @@ It doesn't matter what you leave beyond the new length.
         return output;
     }
 
+    /**
+     * 33. Search in Rotated Sorted Array
+     * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+     */
+    //二分查找，难度在于左右边界的确定
+    int search(vector<int>& nums, int target) {
+        int first = 0, last = nums.size();
+
+        while(first != last){
+            const int mid = first + (last-first)/2;
+
+            if(nums[mid] == target)
+                return mid;
+            if(nums[first] <= nums[mid]){
+                if(nums[first] <= target && target < nums[mid])
+                    last = mid;
+                else
+                    first = mid+1;
+            }else{
+                if(nums[mid] < target && target <= nums[last-1])
+                    first = mid+1;
+                else
+                    last = mid;
+            }
+        }
+
+        return -1;
+    } 
+
     /*
      * 39. 
      * Combination Sum
