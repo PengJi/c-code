bf36d83be6ded5f164dc875028116c8e41a5fb27
pengji <jipeng92@gmail.com>
2018-04-03 19:04:03 +0800

    add 80

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -259,6 +259,46 @@ http://bangbingsyb.blogspot.ca/2014/11/leetcode-merge-intervals.html
         return ret;
     }
 
+    /**
+     * 80. Remove Duplicates from Sorted Array II
+     * Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. 
+It doesn't matter what you leave beyond the new length.
+     */
+    /*
+    加一个变量记录元素出现的次数。如果数据没有排序，则引入一个hashmap来记录出现次数。
+     */
+    //代码一
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() <= 2)
+            return nums.size();
+
+        int index=2;
+        for(int i=2; i<nums.size(); i++){
+            if(nums[i] != nums[index-2])
+                nums[index++] = nums[i];
+        }
+
+        return index;
+    }
+    int removeDuplicates(vector<int>& nums) {
+        const int n = nums.size();
+        int index = 0;
+        for(int i=0;i<n;++i){
+            if(i>0 && i<n-1 && nums[i] == nums[i-1] && nums[i] == nums[i+1])
+                continue;
+
+            nums[index++] = nums[i];
+        }
+
+        return index;
+    }
+
 	/*
 	 * 287. 
 	 * Find the Duplicate Number
