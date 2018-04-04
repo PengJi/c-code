def97216fe3cb1fa3a47f51efb95a3e642ca150d
pengji <jipeng92@gmail.com>
2018-04-04 11:21:16 +0800

    add 15

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -78,6 +78,59 @@ The median is (2 + 3)/2 = 2.5
         return -1;
     }
 
+    /**
+     * 15. 3Sum
+     * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? 
+     * Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+     */
+    //先排序，然后夹逼，跳过重复的数
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        if(nums.size() < 3)
+            return result;
+
+        sort(nums.begin(),nums.end());
+        const int target = 0;
+
+        auto last = nums.end();
+        for(auto i = nums.begin(); i<last-2; ++i){
+            auto j = i+1;
+            if(i>nums.begin() && *i == *(i-1))
+                continue;
+
+            auto k=last-1;
+            while(j<k){
+                if(*i +  *j + *k < target){
+                    ++j;
+                    while(*j == *(j-1) && j<k)
+                        ++j;
+                }else if(*i + *j + *k > target){
+                    --k;
+                    while(*k == *(k+1) && j<k)
+                        --k;
+                }else{
+                    result.push_back({*i,*j,*k});
+                    ++j;
+                    --k;
+                    while(*j == *(j-1) && *k == *(k+1) && j<k)
+                        ++j;
+                }
+            }
+        }
+
+        return result;
+    }
+
     /*
      * 22. 
      * Generate Parentheses
