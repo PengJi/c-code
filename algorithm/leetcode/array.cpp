947fc18a0e7051a134f520312b041d2049a2e7e3
pengji <jipeng92@gmail.com>
2018-04-04 11:09:34 +0800

    add 81

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -195,7 +195,8 @@ You may assume no duplicate exists in the array.
     /*
      * 39. 
      * Combination Sum
-     * Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+     * Given a set of candidate numbers (C) (without duplicates) and a target number (T), 
+     * find all unique combinations in C where the candidate numbers sums to T.
 
 The same repeated number may be chosen from C unlimited number of times.
 
@@ -332,6 +333,47 @@ It doesn't matter what you leave beyond the new length.
         }
 
         return index;
+    }
+
+    /**
+     * 81. Search in Rotated Sorted Array II
+     * Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Write a function to determine if a given target is in the array.
+
+The array may contain duplicates.
+     */
+    bool search(vector<int>& nums, int target) {
+        int first = 0, last = nums.size();
+
+        while(first != last){
+            const int mid = first + (last-first)/2;
+            if(nums[mid] == target)
+                return true;
+
+            if(nums[first] < nums[mid]){
+                if(nums[first] <= target && target < nums[mid])
+                    last = mid;
+                else
+                    first = mid+1;
+            }else if(nums[first] > nums[mid]){
+                if(nums[mid] < target && target <= nums[last-1])
+                    first = mid+1;
+                else
+                    last = mid;
+            }else{
+                first++;
+            }
+        }
+
+        return false;
     }
 
 	/*
