3d5f26d227af9bc5f681022bf532883e674df5ba
pengji <jipeng92@gmail.com>
2018-04-09 18:54:07 +0800

    add 48

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -462,6 +462,74 @@ http://bangbingsyb.blogspot.ca/2014/11/leetcode-combination-sum-i-ii.html
         }
     }
 
+    /**
+     * 48. Rotate Image
+     * 
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+     */
+    //思路一
+    void rotate(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+
+        for(int i=0; i<n; ++i)//沿着副对角线反转
+            for(int j=0; j<n-i; ++j)
+                swap(matrix[i][j], matrix[n-1-j][n-1-i]);
+
+        for(int i=0; i<n/2; ++i)//沿着水平中线反转
+            for(int j=0; j<n; ++j)
+                swap(matrix[i][j], matrix[n-1-i][j]);
+    }
+    //思路二
+    void rotate(vector<vector<int>>& matrix){
+        const int n = matrix.size();
+
+        for(int i=0; i<n/2; ++i)//沿着水平中线反转
+            for(int j=0; i<n; ++j)
+                swap(matrix[i][j], matrix[n-1-i][j]);
+
+        for(int i=0; i<n; ++i) //沿着主对角线反转
+            for(int j=i+1; j<n; ++j)
+                swap(matrix[i][j],matrix[j][i]);
+    }
+
     /*
      * 55. 
      * Jump Game
