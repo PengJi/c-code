51e40feafd2cc68af7f88dbfc3e1294bc58f06fa
pengji <jipeng92@gmail.com>
2018-04-11 20:17:45 +0800

    add 83

--- algorithm/leetcode/list.cpp
+++ algorithm/leetcode/list.cpp
@@ -65,6 +65,53 @@ Output: 7 -> 0 -> 8
     }
 
     /**
+     * 83. Remove Duplicates from Sorted List
+     * Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+     */
+    //递归版
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head)
+            return head;
+        ListNode dummy(head->val+1); //值与head不同即可
+        dummy.next = head;
+
+        recur(&dummy,head);
+        return dummy.next;
+    }
+    static void recur(ListNode *prev, ListNode *cur){
+        if(cur == nullptr)
+            return ;
+
+        if(prev->val == cur->val){
+            prev->next = cur->next;
+            delete cur;
+            recur(prev, prev->next);
+        }else{
+            recur(prev->next, cur->next);
+        }
+    }
+    //迭代版
+    ListNode* deleteDuplicates(ListNode* head){
+        if(head == nullptr)
+            return nullptr;
+
+        for(ListNode *prev = head, *cur=head->next; cur; cur = prev->next){
+            if(prev->val == cur->val){
+                prev->next = cur->next;
+                delete cur;
+            }else{
+                prev=cur;
+            }
+        }
+
+        return head;
+    }
+
+    /**
      * 86. Partition List
      * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
 
