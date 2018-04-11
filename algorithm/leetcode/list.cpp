f4005d8d9011636acd9b77680c82a07df8dd491a
pengji <jipeng92@gmail.com>
2018-04-11 19:54:47 +0800

    add 86

--- algorithm/leetcode/list.cpp
+++ algorithm/leetcode/list.cpp
@@ -65,6 +65,38 @@ Output: 7 -> 0 -> 8
     }
 
     /**
+     * 86. Partition List
+     * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+For example,
+Given 1->4->3->2->5->2 and x = 3,
+return 1->2->2->4->3->5.
+     */
+    ListNode* partition(ListNode* head, int x) {
+        ListNode left_dummy(-1); //头结点
+        ListNode right_dummy(-1); //头结点
+
+        auto left_cur = &left_dummy;
+        auto right_cur = &right_dummy;
+
+        for(ListNode *cur = head; cur; cur = cur->next){
+            if(cur->val < x){
+                left_cur->next = cur;
+                left_cur = cur;
+            }else{
+                right_cur->next = cur;
+                right_cur = cur;
+            }
+        }
+
+        left_cur->next = right_dummy.next;
+        right_cur->next = nullptr;
+        return left_dummy.next;
+    }
+
+    /**
      * 92. Reverse Linked List II
      * Reverse a linked list from position m to n. Do it in-place and in one-pass.
      *
