7a08b211bb103655ebe099f1f30a617e5e85a7f6
pengji <jipeng92@gmail.com>
2018-04-12 18:15:51 +0800

    add 82

--- algorithm/leetcode/list.cpp
+++ algorithm/leetcode/list.cpp
@@ -65,6 +65,65 @@ Output: 7 -> 0 -> 8
     }
 
     /**
+     * 82. Remove Duplicates from Sorted List II
+     * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+     */
+    //递归版
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+
+        ListNode *p = head->next;
+        if(head->val == p->val){
+            while(p && head->val == p->val){
+                ListNode *tmp = p;
+                p = p->next;
+                delete tmp;
+            }
+            delete head;
+            return deleteDuplicates(p);
+        }else{
+            head->next = deleteDuplicates(head->next);
+            return head;
+        }
+    }
+    //迭代版
+    ListNode *deleteDuplicates(ListNode *head){
+        if(head == nullptr)
+            return head;
+
+        ListNode dummy(INT_MIN); //头结点
+        dummy.next = head;
+        ListNode *prev = &dummy, *cur = head;
+        while(cur != nullptr){
+            bool duplicated = false;
+            while(cur->next != nullptr && cur->val == cur->next->val){
+                duplicated = true;
+                ListNode *temp = cur;
+                cur = cur->next;
+                delete temp;
+            }
+            if(duplicated){
+                ListNode *temp = cur;
+                cur = cur->next;
+                delete temp;
+                continue;
+            }
+
+            prev->next = cur;
+            prev = prev->next;
+            cur = cur->next;
+        }
+
+        prev->next = cur;
+        return dummy.next;
+    }
+
+    /**
      * 83. Remove Duplicates from Sorted List
      * Given a sorted linked list, delete all duplicates such that each element appear only once.
 
