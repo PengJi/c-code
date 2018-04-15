8b4d37a3f387d926ee6761430e68645016cd9a45
pengji <jipeng92@gmail.com>
2018-04-15 19:42:38 +0800

    add 183

--- algorithm/leetcode/list.cpp
+++ algorithm/leetcode/list.cpp
@@ -436,8 +436,31 @@ Given m, n satisfy the following condition:
 
 Return a deep copy of the list.
      */
+    //扫描两遍
     RandomListNode *copyRandomList(RandomListNode *head) {
-        
+        for(RandomListNode *cur = head; cur != nullptr; ){
+            RandomListNode *node = new RandomListNode(cur->label);
+            node->next = cur->next;
+            cur->next = node;
+            cur = node->next;
+        }
+
+        for(RandomListNode* cur=head; cur!=nullptr;){
+            if(cur->random != NULL)
+                cur->next->random = cur->random->next;
+            cur = cur->next->next;
+        }
+
+        //分拆两个单链表
+        RandomListNode dummy(-1);
+        for(RandomListNode *cur = head, *new_cur = &dummy; cur!=nullptr; ){
+            new_cur->next = cur->next;
+            new_cur = new_cur->next;
+            cur->next = cur->next->next;
+            cur = cur->next;
+        }
+
+        return dummy.next;
     }
 
     /**
