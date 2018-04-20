a4c782aa96bcd4c1f9045df26896e9518ab279a7
pengji <jipeng92@gmail.com>
2018-04-20 19:27:29 +0800

    add 14

--- algorithm/leetcode/string.cpp
+++ algorithm/leetcode/string.cpp
@@ -23,6 +23,54 @@ Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer 
     }
 
     /**
+     * 14. Longest Common Prefix
+     * Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+Note:
+
+All given inputs are in lowercase letters a-z.
+     */
+    //从位置0开始，对每一个位置比较所有字符串，直到遇到一个不匹配
+    //纵向扫描，从位置0开始，对每一个位置比较所欲字符串，直到遇到一个不匹配
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty())
+            return "";
+
+        for(int idx=0; idx<strs[0].size(); ++idx){ //纵向扫描
+            for(int i=1; i<strs.size(); ++i){
+                if(strs[i][idx] != strs[0][idx])
+                    return strs[0].substr(0,idx);
+            }
+        }
+
+        return strs[0];
+    }
+    //横向扫描，每个字符串与地0个字符串，从左到右比较，直到遇到一个不匹配
+    string longestCommonPrefix(vector<string>& strs){
+        if(strs.empty())
+            return "";
+
+        int right_most = strs[0].size()-1;
+        for(size_t i=1; i<strs.size(); i++)
+            for(int j=0; j<=right_most; j++)
+                if(strs[i][j] != strs[0][j])
+                    right_most = j-1;
+
+        return strs[0].substr(0, right_most+1);
+    }
+
+    /**
      * 28. Implement strStr()
      * Implement strStr().
 
