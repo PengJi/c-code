f22f21c9ec5f99640761225965befd925b3bab99
pengji <jipeng92@gmail.com>
2018-04-21 19:03:47 +0800

    add 85

--- algorithm/leetcode/dp.cpp
+++ algorithm/leetcode/dp.cpp
@@ -49,6 +49,57 @@ http://bangbingsyb.blogspot.ca/2014/11/leetcode-longest-palindromic-substring.ht
     }
 
     /**
+     * 85. Maximal Rectangle
+     * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+
+Example:
+
+Input:
+[
+  ["1","0","1","0","0"],
+  ["1","0","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","0","0","1","0"]
+]
+Output: 6
+     */
+	int maximalRectangle(vector<vector<char>>& matrix) {
+		if(matrix.empty())
+			return 0;
+
+		const int m = matrix.size();
+		const int n = matrix[0].size();
+		vector<int> H(n,0);
+		vector<int> L(n,0);
+		vector<int> R(n,n);
+
+		int ret = 0;
+		for(int i=0; i<m; ++i){
+			int left = 0, right = n;
+			for(int j=0; j<n; ++j){
+				if(matrix[i][j] == '1'){
+					++H[j];
+					L[j] = max(L[j],left);
+				}else{
+					left = j+1;
+					H[j] = 0; L[j]=0; R[j]=n;
+				}
+			}
+
+			for(int j=n-1; j>=0; --j){
+				if(matrix[i][j] == '1'){
+					R[j] = min(R[j],right);
+					ret = max(ret, H[j]*(R[j]-L[j]));
+				}else{
+					right = j;
+				}
+			}
+		}
+
+		return ret;
+	}
+
+    /**
      * 120. Triangle
      * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
 
