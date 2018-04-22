871caa3bfac1d691b6269ae91949638330c9ace5
pengji <jipeng92@gmail.com>
2018-04-22 19:03:32 +0800

    add 91

--- algorithm/leetcode/dp.cpp
+++ algorithm/leetcode/dp.cpp
@@ -101,6 +101,49 @@ Output: 6
 		return ret;
 	}
 
+	/**
+	 * 91. Decode Ways
+	 * A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be deodced as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+	 */
+	int numDecodings(string s) {
+		if(s.empty() || s[0]=='0')
+			return 0;
+
+		int prev = 0;
+		int cur = 1;
+		//长度为n的字符串，有n+1个阶梯
+		for(size_t i=1; i<=s.size(); ++i){
+			if(s[i-1] == '0')
+				cur = 0;
+
+			if(i<2 | !(s[i-2] == '1' || (s[i-2] == '2' && s[i-1] <= '6')))
+				prev = 0;
+
+			int tmp = cur;
+			cur = prev+cur;
+			prev = tmp;
+		}
+
+		return cur;
+	}
+
     /**
      * 120. Triangle
      * Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
