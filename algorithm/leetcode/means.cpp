705065cd9466fe30b49756384a36e0956ac4643d
pengji <jipeng92@gmail.com>
2018-04-26 18:33:40 +0800

    add 29

--- algorithm/leetcode/means.cpp
+++ algorithm/leetcode/means.cpp
@@ -88,6 +88,45 @@ Coud you solve it without converting the integer to a string?
 	}
 
 	/**
+	 * 29. Divide Two Integers
+	 * Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero.
+
+Example 1:
+Input: dividend = 10, divisor = 3
+Output: 3
+
+Example 2:
+Input: dividend = 7, divisor = -3
+Output: -2
+Note:
+
+Both dividend and divisor will be 32-bit signed integers.
+The divisor will never be 0.
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: 
+[−231,  231 − 1]. For the purpose of this problem, assume that your function returns 231 − 1 when the division result overflows.
+	 */
+	//不能用乘、除和取模，还有加、减和位运算
+	int divide(int dividend, int divisor) {
+		if(dividend == 0)
+			return 0;
+		if(divisor == 0)
+			return INT_MAX;
+
+		long long res = double(exp(log(fabs(dividend)) - log(fabs(divisor))));
+
+		if((dividend < 0) ^ (divisor < 0))
+			res = -res;
+		if(res>INT_MAX)
+			res = INT_MAX;
+
+		return res;
+	}
+
+	/**
 	 * 50. Pow(x, n)
 	 * 
 Implement pow(x, n), which calculates x raised to the power n (xn).
