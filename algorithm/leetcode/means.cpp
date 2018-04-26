aee9001103c7a97344a23f69c38d376f184b1f37
pengji <jipeng92@gmail.com>
2018-04-26 17:25:15 +0800

    modify means.cpp

--- algorithm/leetcode/means.cpp
+++ algorithm/leetcode/means.cpp
@@ -88,40 +88,6 @@ Coud you solve it without converting the integer to a string?
 	}
 
 	/**
-	 * 78. Subsets
-	 * Given a set of distinct integers, nums, return all possible subsets (the power set).
-
-Note: The solution set must not contain duplicate subsets.
-
-Example:
-
-Input: nums = [1,2,3]
-Output:
-[
-  [3],
-  [1],
-  [2],
-  [1,2,3],
-  [1,3],
-  [2,3],
-  [1,2],
-  []
-]
-	 */
-	vector<vector<int>> subsets(vector<int> &nums){
-		sort(nums.begin(), nums.end());
-		int elem_num = nums.size();
-		int subset_num = pow(2,elem_num);
-		vector<vector<int>> subset_set(subset_num, vector<int>());
-		for(int i=0; i<elem_num; i++)
-			for(int j=0; j<subset_num; j++)
-				if((j >> i) & 1)
-					subset_set[j].push_back(nums[i]);
-
-		return subset_set;
-	}
-
-	/**
 	 * 50. Pow(x, n)
 	 * 
 Implement pow(x, n), which calculates x raised to the power n (xn).
@@ -329,6 +295,40 @@ Output:
 		}
 
 		return matrix;
+	}
+
+	/**
+	 * 78. Subsets
+	 * Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+	 */
+	vector<vector<int>> subsets(vector<int> &nums){
+		sort(nums.begin(), nums.end());
+		int elem_num = nums.size();
+		int subset_num = pow(2,elem_num);
+		vector<vector<int>> subset_set(subset_num, vector<int>());
+		for(int i=0; i<elem_num; i++)
+			for(int j=0; j<subset_num; j++)
+				if((j >> i) & 1)
+					subset_set[j].push_back(nums[i]);
+
+		return subset_set;
 	}
 
 	/**
