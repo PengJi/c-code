4ae951f87be0da4efa8d1fe503eab09cf7935758
pengji <jipeng92@gmail.com>
2018-05-01 10:18:51 +0800

    rename file

--- algorithm/leetcode/means.cpp
+++ algorithm/leetcode/means.cpp
@@ -400,40 +400,6 @@ Output:
 	}
 
 	/**
-	 * 78. Subsets
-	 * Given a set of distinct integers, nums, return all possible subsets (the power set).
-
-Note: The solution set must not contain duplicate subsets.
-
-Example:
-
-Input: nums = [1,2,3]
-Output:
-[
-  [3],
-  [1],
-  [2],
-  [1,2,3],
-  [1,3],
-  [2,3],
-  [1,2],
-  []
-]
-	 */
-	vector<vector<int>> subsets(vector<int> &nums){
-		sort(nums.begin(), nums.end());
-		int elem_num = nums.size();
-		int subset_num = pow(2,elem_num);
-		vector<vector<int>> subset_set(subset_num, vector<int>());
-		for(int i=0; i<elem_num; i++)
-			for(int j=0; j<subset_num; j++)
-				if((j >> i) & 1)
-					subset_set[j].push_back(nums[i]);
-
-		return subset_set;
-	}
-
-	/**
 	 * 118. Pascal's Triangle
 	 * Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
 
