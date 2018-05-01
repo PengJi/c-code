36120cf9ca3782f0c3b4876fba18b1dbe86e7762
pengji <jipeng92@gmail.com>
2018-05-01 16:26:57 +0800

    add 17

--- algorithm/leetcode/brute_force.cpp
+++ algorithm/leetcode/brute_force.cpp
@@ -18,9 +18,52 @@ Note:
 Although the above answer is in lexicographical order, your answer could be in any order you want.
 	 */
 	//递归
-	const vector<string> keyboard
+	const vector<string> keyboard {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 	vector<string> letterCombinations(string digits) {
+		vector<string> result;
+		if(digits.empty())
+			return result;
 
+		dfs(digits, 0, "", result);
+
+		return result;
+	}
+	void dfs(const string &digits, size_t cur, string path,
+		vector<string> &result){
+		if(cur == digits.size()){
+			result.push_back(path);
+			return;
+		}
+
+		for(auto c : keyboard[digits[cur] - '0']){
+			dfs(digits, cur+1, path+c, result);
+		}
+	}
+
+	//迭代
+	const vector<string> keyboard {" ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+	vector<string> letterCombinations(string digits){
+		if(digits.empty())
+			return vector<string>();
+
+		vector<string> result(1, "");
+		for(auto d:digits){
+			const size_t n = result.size();
+			const size_t m = keyboard[d - '0'].size();
+
+			result.resize(n * m);
+			for(size_t i = 0; i<m; ++i)
+				copy(result.begin(), result.begin()+n, result.begin()+n*i);
+
+			for(size_t i=0; i<m; ++i){
+				auto begin = result.begin();
+				for_each(begin+n*i, begin+n*(i+1), [&](string &s){
+					s += keyboard[d-'0'][i];
+				});
+			}
+		}
+
+		return result;
 	}
 
 	/**
