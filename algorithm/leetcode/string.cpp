00bbc572c6c2297a5a13b9b26fb110780b651482
pengji <jipeng92@gmail.com>
2018-05-02 20:46:27 +0800

    add 67

--- algorithm/leetcode/string.cpp
+++ algorithm/leetcode/string.cpp
@@ -292,11 +292,23 @@ Output: "10101"
      */
     string addBinary(string a, string b) {
         string result;
-        const size_t n = a.size() > b.szie() ? a.size() : b.size();
+        const size_t n = a.size() > b.size() ? a.size() : b.size();
         reverse(a.begin(), a.end());
         reverse(b.begin(), b.end());
         int carry = 0;
-        for()
+        for(size_t i=0; i<n; i++){
+            const int ai = i < a.size() ? a[i] - '0' : 0;
+            const int bi = i < b.size() ? b[i] - '0' : 0;
+            const int val = (ai + bi + carry) % 2;
+            carry = (ai + bi + carry) / 2;
+            result.insert(result.begin(), val+'0');
+        }
+
+        if(carry == 1){
+            result.insert(result.begin(), '1');
+        }
+
+        return result;
     }
 
     /**
