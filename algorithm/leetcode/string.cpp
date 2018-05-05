aa946659694214f25c7bc8ca8ec26f79afecbcd3
pengji <jipeng92@gmail.com>
2018-05-05 20:54:06 +0800

    add 12

--- algorithm/leetcode/string.cpp
+++ algorithm/leetcode/string.cpp
@@ -174,6 +174,118 @@ Output: false
     }
 
     /**
+     * 12. Integer to Roman
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. 
+Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, 
+the numeral for four is not IIII. Instead, the number four is written as IV. 
+Because the one is before the five we subtract it making four. The same principle applies to the number nine, 
+which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+Input: 3
+Output: "III"
+
+Example 2:
+Input: 4
+Output: "IV"
+
+Example 3:
+Input: 9
+Output: "IX"
+
+Example 4:
+Input: 58
+Output: "LVIII"
+Explanation: C = 100, L = 50, XXX = 30 and III = 3.
+
+Example 5:
+Input: 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+     */
+    string intToRoman(int num) {
+        const int radix[] = {1000, 900, 500, 400, 100, 90,
+            50, 40, 10, 9, 5, 4, 1};
+        const string symbol[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X",
+            "IX", "V", "IV", "I"};
+
+        string roman;
+        for(size_t i = 0; num>0; ++i){
+            int count = num / radix[i];
+            num %= radix[i];
+            for(; count>0; --count)
+                roman += symbol[i];
+        }
+        return roman;
+    }
+
+    /**
+     * 13. Roman to Integer
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, two is written as II in Roman numeral, just two one's added together. 
+Twelve is written as, XII, which is simply X + II. The number etwenty sevn is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. 
+However, the numeral for four is not IIII. Instead, the number four is written as IV. 
+Because the one is before the five we subtract it making four. 
+The same principle applies to the number nine, which is written as IX. 
+There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+Input: "III"
+Output: 3
+
+Example 2:
+Input: "IV"
+Output: 4
+
+Example 3:
+Input: "IX"
+Output: 9
+
+Example 4:
+Input: "LVIII"
+Output: 58
+Explanation: C = 100, L = 50, XXX = 30 and III = 3.
+
+Example 5:
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+     */
+
+    /**
      * 14. Longest Common Prefix
 Write a function to find the longest common prefix string amongst an array of strings.
 
@@ -470,6 +582,9 @@ Note: It is intended for the problem statement to be ambiguous. You should gathe
 Update (2015-02-10):
 The signature of the C++ function had been updated. 
 If you still see your function signature accepts a const char * argument, please click the reload button to reset your code definition.
+
+Solution:
+http://www.cnblogs.com/grandyang/p/4084408.html
      */
     bool isNumber(string s) {
         int len = s.size();
