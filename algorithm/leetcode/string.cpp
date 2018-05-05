a4ac411167c19a388283d3cc8a830c72601350e0
pengji <jipeng92@gmail.com>
2018-05-05 21:02:20 +0800

    add 13

--- algorithm/leetcode/string.cpp
+++ algorithm/leetcode/string.cpp
@@ -284,6 +284,35 @@ Input: "MCMXCIV"
 Output: 1994
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
      */
+    //从前往后扫描，用一个临时变量记录分段数字。
+    //如果当前比前一个大，说明这一段的值应该是当前值减去一个值。
+    //比如：IV = 5-1; 
+    //否则，将当前值加入到结果中，然后开始下一段记录。
+    //比如：VI = 5+1， II=1+1;
+    inline int map(const char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    int romanToInt(string s) {
+        int result = 0;
+        for(size_t i = 0; i<s.size(); i++){
+            if(i>0 && map(s[i]) > map(s[i-1])){
+                result += (map(s[i]) - 2*map(s[i-1]));
+            }else{
+                result += map(s[i]);
+            }
+        }
+
+        return result;
+    }
 
     /**
      * 14. Longest Common Prefix
