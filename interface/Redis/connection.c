027279eaaa47a48a87d8e43658753bc2abe4d13b
pengji <jipeng92@gmail.com>
2018-05-12 11:48:07 +0800

    add connection.c

---
+++ interface/Redis/connection.c
@@ -0,0 +1,63 @@
+#include <hiredis/hiredis.h>
+
+/**
+ * hiredis连接
+ */
+
+int main(int argc, char **argv) {
+   redisReply *reply;
+   // Connecting
+   redisContext *c = redisConnect("127.0.0.1", 6001);
+
+   if (c == NULL || c->err) {
+      if (c) {
+         printf("Connecting Error: %s\n", c->errstr);
+      } else {
+         printf("Can't allocate redis context\n");
+      }   
+      return -1; 
+   }   
+   // SET command
+   reply = redisCommand(c, "SET key Hello");
+   if (reply == NULL) {
+      printf("redisCommand reply is NULL: %s\n", c->errstr);
+      return -1; 
+   }   
+   if (reply->type == REDIS_REPLY_ERROR) {
+      printf("Command Error: %s\n", reply->str);
+      freeReplyObject(reply);
+      return -1; 
+   }   
+   printf("Command reply: %s\n", reply->str);
+   freeReplyObject(reply);
+	
+   // GET command
+   reply = redisCommand(c, "GET key");
+   if (reply == NULL) {
+      printf("redisCommand reply is NULL\n");
+      return -1; 
+   }
+   if (reply->type == REDIS_REPLY_ERROR) {
+      printf("Command Error: %s\n", reply->str);
+      freeReplyObject(reply);
+      return -1;
+   }
+   printf("Command reply: %s\n", reply->str);
+   freeReplyObject(reply);
+
+   // GET XXX
+   reply = redisCommand(c, "GET XXX");
+   if (reply == NULL) {
+      printf("redisCommand reply is NULL\n");
+      return -1; 
+   }   
+   if (reply->type == REDIS_REPLY_ERROR) {
+      printf("Command Error: %s\n", reply->str);
+      freeReplyObject(reply);
+      return -1; 
+   }   
+   printf("Command reply: %s\n", reply->str);
+   freeReplyObject(reply);
+   
+   redisFree(c);
+}
