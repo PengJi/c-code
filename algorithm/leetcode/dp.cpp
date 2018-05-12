0927a68866bf38550905e31e6dc5422514aaf48b
pengji <jipeng92@gmail.com>
2018-05-12 20:06:05 +0800

    add 64

--- algorithm/leetcode/dp.cpp
+++ algorithm/leetcode/dp.cpp
@@ -69,6 +69,96 @@ If you have figured out the O(n) solution, try coding another solution using the
 		return result;
 	}
 
+	/**
+	 * 64. Minimum Path Sum
+Given a m x n grid filled with non-negative numbers, 
+find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+	 */
+	//设状态为f[i][j]，表示从起点(0,0)到达(i,j)的最小路径和，
+	//则状态转移方程为:f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j]
+	//备忘录法
+    int minPathSum(vector<vector<int>>& grid) {
+    	const int m = grid.size();
+    	const int n = grid[0].size();
+    	this->f = vector<vector<int>> (m, vector<int>(n, -1));
+    	return dfs(grid, m-1, n-1);
+    }
+    vector<vector<int>> f; //缓存
+    int dfs(const vector<vector<int>> &grid, int x, int y){
+    	if(x<0 || y<0) //越界，终止条件
+    		return INT_MAX;
+
+    	if(x==0 && y==0) //回到起点，收敛条件
+    		return grid[0][0];
+
+    	return min(getOrUpdate(grid, x-1, y), getOrUpdate(grid, x, y-1))+grid[x][y];
+    }
+    int getOrUpdate(const vector<vector<int>> &grid, int x, int y){
+    	if(x<0 || y<0)
+    		return INT_MAX;
+    	if(f[x][y] >= 0)
+    		return f[x][y];
+    	else
+    		return f[x][y] = dfs(grid,x,y);
+    }
+    //动态规划
+    int minPathSum(vector<vector<int>> &grid){
+    	if(grid.size() == 0)
+    		return 0;
+    	const int m = grid.size();
+    	const int n = grid[0].size();
+
+    	int f[m][n];
+    	f[0][0] = grid[0][0];
+    	for(int i=1; i<m; i++){
+    		f[i][0] = f[i-1][0] + grid[i][0];
+    	}
+    	for(int i=1; i<n; i++){
+    		f[0][i] = f[0][i-1] + grid[0][i];
+    	}
+
+    	for(int i=1; i<m; i++){
+    		for(int j=1; j<n; j++){
+    			f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j];
+    		}
+    	}
+
+    	return f[m-1][n-1];
+    }
+    //动态规划+滚动数组
+    int minPathSum(vector<vector<int>> &grid){
+    	const int m = grid.size();
+    	const int n = grid[0].size();
+
+    	int f[n];
+    	fill(f, f+n, INT_MAX);
+    	f[0] = 0;
+
+    	for(int i=0; i<m; i++){
+    		f[0] += grid[i][0];
+    		for(int j=1; j<n; j++){
+    			//左边的f[j]，表示更新后的f[j]，与公式中的f[i][j]对应
+    			//右边的f[j]，表示老的f[j]，与公式中的f[i-1][j]对应
+    			f[j] = min(f[j-1], f[j]) + grid[i][j];
+    		}
+    	}
+
+    	return f[n-1];
+    }
+
     /**
      * 85. Maximal Rectangle
 Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
