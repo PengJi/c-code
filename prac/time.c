f60b542a42218b3ebede5cee7134578701f5e7a6
pengji <jipeng92@gmail.com>
2018-05-14 15:58:42 +0800

    modify time.c

--- prac/time.c
+++ prac/time.c
@@ -6,26 +6,26 @@
 int main()
 {
 	//第一种方法，linux环境下
-	struct  timeval    tv;
-	struct  timezone   tz;
-	gettimeofday(&tv,&tz);
-	struct  timeval start;
-	struct  timeval end;
+	struct timeval    tv;
+	struct timezone   tz;
  
+ 	gettimeofday(&tv,&tz);
 	printf("tv_sec:%d\n",tv.tv_sec);
 	printf("tv_usec:%d\n",tv.tv_usec);
 	printf("tz_minuteswest:%d\n",tz.tz_minuteswest);
 	printf("tz_dsttime:%d\n",tz.tz_dsttime);
 
-	unsigned  long diff;
+	// 计算间隔时间
+	struct timeval start, end;
+	unsigned long diff;
 	gettimeofday(&start,NULL);
 	printf("test interval\n");
 	sleep(5);
 	gettimeofday(&end,NULL);
-	diff = 1000000 * (end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec;
-	printf("the distance is %ld us\n",diff);
-	printf("the distance is %ld ms\n",diff/1000);
-	printf("the distance is %d s\n",diff/1000000);
+	diff = 1000000 * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec;
+	printf("the distance is %ld us\n",diff); //微妙
+	printf("the distance is %ld ms\n",diff/1000); //毫秒
+	printf("the distance is %d s\n",diff/1000000); //秒
 
 	//第二种方法(linux下失效)
 	clock_t startTime, endTime;
