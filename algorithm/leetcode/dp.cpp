9b47ecf695e52bd8283a08c685dd2c83ae90d4f6
pengji <jipeng92@gmail.com>
2018-05-16 22:02:16 +0800

    add 139

--- algorithm/leetcode/dp.cpp
+++ algorithm/leetcode/dp.cpp
@@ -582,6 +582,36 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 	}
 
 	/**
+	 * 139. Word Break
+Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, 
+determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+
+Example 1:
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false
+	 */
+	//设状态为f(i)，表示s[0,i]是否可以分词，则状态转移方程为
+	//f(i) = any_of(f(i) && s[j+1, i] 属于 dict), 0 <= j < i
+	//动态规划
+	bool wordBreak(string s, vector<string>& wordDict) {
+	}
+
+	/**
 	 * 221. Maximal Square
 Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
 
