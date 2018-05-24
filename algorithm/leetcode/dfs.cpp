c4bef3e3f334f87b6fd76f5640ade85eccf975b7
pengji <jipeng92@gmail.com>
2018-05-24 19:57:36 +0800

    add 40

--- algorithm/leetcode/dfs.cpp
+++ algorithm/leetcode/dfs.cpp
@@ -1,6 +1,66 @@
 class Solution {
 public:
 	/**
+	 * 40. Combination Sum II
+Given a collection of candidate numbers (candidates) and a target number (target), 
+find all unique combinations in candidates where the candidate numbers sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+
+Example 1:
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+
+Example 2:
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+	 */
+	vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+		sort(candidates.begin(), candidates.end());
+
+		vector<vector<int>> result;
+		vector<int> path;
+		dfs(candidates, path, result, target, 0);
+
+		return result;
+	}
+	void dfs(const vector<int> &candidates, vector<int> &path, 
+		vector<vector<int>> &result, int gap, int start){
+		if(gap == 0){ //找到一个合法解
+			result.push_back(path);
+			return;
+		}
+
+		int previous = -1;
+		for(size_t i=start; i<candidates.size(); ++i){
+			//candidates[i]最多只用一次
+			if(previous == candidates[i]) continue;
+
+			if(gap < candidates[i]) return; //剪枝
+
+			previous = candidates[i];
+
+			path.push_back(candidates[i]);
+			dfs(candidates, path, result, gap-candidates[i], i+1);
+			path.pop_back(); //恢复环境
+		}
+	}
+
+	/**
 	 * 62. Unique Paths
 A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
 
