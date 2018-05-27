55abc707b7242a5bf094f7214834ad07c9775743
pengji <jipeng92@gmail.com>
2018-05-27 20:12:24 +0800

    add 105

--- algorithm/leetcode/tree.cpp
+++ algorithm/leetcode/tree.cpp
@@ -468,6 +468,46 @@ return its depth = 3.
   }
 
   /**
+   * 105. Construct Binary Tree from Preorder and Inorder Traversal
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+   */
+  TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    return buildTree(begin(preorder), end(preorder), 
+      begin(inorder), end(inorder));
+  }
+  template<typename InputIterator>
+  TreeNode *buildTree(InputIterator pre_first, InputIterator pre_last,
+    InputIterator in_first, InputIterator in_last){
+    if(pre_first == pre_last) return nullptr;
+    if(in_first == in_last) return nullptr;
+
+    auto root = new TreeNode(*pre_first);
+
+    auto inRootPos = find(in_first, in_last, *pre_first);
+    auto leftSize = distance(in_first, inRootPos);
+
+    root->left = buildTree(next(pre_first), next(pre_first,
+      leftSize + 1), in_first, next(in_first, leftSize));
+    root->right = buildTree(next(pre_first, leftSize+1), pre_last,
+      next(inRootPos), in_last);
+
+    return root;
+  }
+
+  /**
    * 107. Binary Tree Level Order Traversal II
 Given a binary tree, return the bottom-up level order traversal of its nodes' values. 
 (ie, from left to right, level by level from leaf to root).
