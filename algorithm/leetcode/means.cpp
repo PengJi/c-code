bed6456bf7d87eb4589e35125c0734a6e40ae5dc
pengji <jipeng92@gmail.com>
2018-05-28 19:05:44 +0800

    add 6

--- algorithm/leetcode/means.cpp
+++ algorithm/leetcode/means.cpp
@@ -5,6 +5,55 @@
 class Solution{
 public:
 	/**
+	 * 6. ZigZag Conversion
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number 
+of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+
+Example 1:
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+Example 2:
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+problem:
+https://leetcode.com/problems/zigzag-conversion/description/
+	//对于每一层垂直元素的坐标(i,j) = (j+1)*n+i；
+	//对于每两层垂直元素之间的插入元素(斜对角元素)，(i,j)=(j+1)*n-1;
+	 */
+	string convert(string s, int numRows) {
+        if(numRows <=1 || s.size()<=1) return s;
+        string result;
+
+        for(int i=0; i<numRows; i++){
+        	for(int j=0,index=i; index<s.size(); j++, index=(2*numRows-2)*j+i){
+        		result.append(1,s[index]); //垂直元素
+        		if(i==0 || i==numRows-1) continue;
+        		if(index + (numRows-i-1) *2 < s.size())
+        			result.append(1,s[index+(numRows-i-1)*2]);
+        	}
+        }
+
+        return result;
+    }
+
+	/**
 	 * 7. Reverse Integer
 Given a 32-bit signed integer, reverse digits of an integer.
 
