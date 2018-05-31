dbc02da9aae8ce5f293ab9d6e8ad52d418760df4
pengji <jipeng92@gmail.com>
2018-05-31 19:39:45 +0800

    add 93

--- algorithm/leetcode/dfs.cpp
+++ algorithm/leetcode/dfs.cpp
@@ -267,6 +267,49 @@ There are two ways to reach the bottom-right corner:
 	}
 
 	/**
+	 * 93. Restore IP Addresses
+Given a string containing only digits, restore it by returning all possible valid 
+IP address combinations.
+
+Example:
+Input: "25525511135"
+Output: ["255.255.11.135", "255.255.111.35"]
+
+problem:
+https://leetcode.com/problems/restore-ip-addresses/description/
+	 */
+	vector<string> restoreIpAddresses(string s) {
+		vector<string> result;
+		vector<string> ip;
+		dfs(s, ip, result, 0);
+		return result;
+	}
+	void dfs(string s, vector<string>& ip, vector<string>& result, size_t start){
+		if(ip.size() == 4 && start==s.size()){
+			result.push_back(ip[0] + '.' + ip[1] + '.' + ip[2] + '.' +ip[3]);
+			return;
+		}
+
+		if(s.size() - start > (4-ip.size()) * 3)
+			return; //剪枝
+		if(s.size() - start < (4-ip.size()))
+			return; //剪枝
+
+		int num=0;
+		for(size_t i=start; i<start+3; i++){
+			num = num*10 + (s[i] - '0');
+
+			if(num < 0 || num>255) continue; //剪枝
+
+			ip.push_back(s.substr(start, i-start+1));
+			dfs(s, ip, result, i+1);
+			ip.pop_back();
+
+			if(num == 0) break;
+		}
+	}
+
+	/**
 	 * 131. Palindrome Partitioning
 Given a string s, partition s such that every substring of the partition is a palindrome.
 Return all possible palindrome partitioning of s.
