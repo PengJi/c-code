7b91f500394da061517e70f019e50894380e9148
pengji <jipeng92@gmail.com>
2018-06-03 18:30:18 +0800

    add 87

--- algorithm/leetcode/dp.cpp
+++ algorithm/leetcode/dp.cpp
@@ -306,6 +306,91 @@ Output: 6
 	}
 
 	/**
+	 * 87. Scramble String
+Given a string s1, we may represent it as a binary tree by partitioning it to 
+two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, 
+it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", 
+it produces a scrambled string "rgtae".
+
+    rgtae
+   /    \
+  rg    tae
+ / \    /  \
+r   g  ta  e
+       / \
+      t   a
+We say that "rgtae" is a scrambled string of "great".
+
+Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+
+Example 1:
+Input: s1 = "great", s2 = "rgeat"
+Output: true
+
+Example 2:
+Input: s1 = "abcde", s2 = "caebd"
+Output: false
+
+problem:
+https://leetcode.com/problems/scramble-string/description/
+	 */
+	//动规
+	//设状态f[n][i][j]表示长度为n，起点为s1[i]和起点为s2[j]两个字符串是否互为scramble，则状态转移方程为
+	//f[n][i][j] = (f[k][i][j] && f[n-k][i+k][j+k])
+	//				|| (f[k][i][j+n-k] && f[n-k][i+k][j])
+	bool isScramble(string s1, string s2) {
+        const int N = s1.size();
+        if(N != s2.size()) return false;
+
+        bool f[N+1][N][N];
+        fill_n(&f[0][0][0], (N+1)*N*N, false);
+
+        for(int i=0; i<N; i++)
+        	for(int j=0; j<N; j++)
+        		f[1][i][j] = s1[i] == s2[j];
+
+        for(int n=1; n<=N; ++n){
+        	for(int i=0; i+n<=N; ++i){
+        		for(int j=0; j+n<=N; ++j){
+        			for(int k=1; k<n; ++k){
+        				if((f[k][i][j] && f[n-k][i+k][j+k]) 
+        					|| (f[k][i][j+n-k]) && f[n-k][i+k][j]){
+        					f[n][i][j] = true;
+        					break;
+        				}
+        			}
+        		}
+        	}
+        }
+
+        return f[N][0][0];
+    }
+
+	/**
 	 * 91. Decode Ways
 A message containing letters from A-Z is being encoded to numbers using the following mapping:
 'A' -> 1
