f40b987b65e14225a0f00a2015d6fb88e51955a3
pengji <jipeng92@gmail.com>
2018-06-06 20:29:47 +0800

    add 160

--- algorithm/leetcode/list.cpp
+++ algorithm/leetcode/list.cpp
@@ -685,6 +685,50 @@ https://www.nowcoder.com/questionTerminal/d75c232a0405427098a8d1627930bea6
     }
 
     /**
+     * 160. Intersection of Two Linked Lists
+Write a program to find the node at which the intersection of 
+two singly linked lists begins.
+
+For example, the following two linked lists:
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+Notes:
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+
+problem:
+https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+solution:
+https://leetcode.com/problems/intersection-of-two-linked-lists/discuss/49789/My-accepted-simple-and-shortest-C++-code-with-comments-explaining-the-algorithm.-Any-comments-or-improvements
+     */
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *p1 = headA;
+        ListNode *p2 = headB;
+
+        if(p1 == NULL || p2 == NULL) return NULL;
+
+        while(p1 != NULL && p2 != NULL && p1!=p2){
+            p1 = p1->next;
+            p2 = p2->next;
+
+            if(p1 == p2) return p1;
+
+            if(p1 == NULL) p1 = headB;
+            if(p2 == NULL) p2 = headA;
+        }
+
+        return p1;
+    }
+
+    /**
      * 206. Reverse Linked List
      * Reverse a singly linked list.
      */
