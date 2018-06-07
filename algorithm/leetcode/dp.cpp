d1ccc78ef2fdd1f880b21b8499d29f5d21db618f
pengji <jipeng92@gmail.com>
2018-06-07 19:15:58 +0800

    add 198

--- algorithm/leetcode/dp.cpp
+++ algorithm/leetcode/dp.cpp
@@ -801,6 +801,41 @@ Output:
     	return out;
     }
 
+    /**
+     * 198. House Robber
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, the only constraint 
+stopping you from robbing each of them is that adjacent houses have 
+security system connected and it will automatically contact the police 
+if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money 
+of each house, determine the maximum amount of money you can rob tonight 
+without alerting the police.
+
+Example 1:
+Input: [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+             Total amount you can rob = 1 + 3 = 4.
+
+Example 2:
+Input: [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+             Total amount you can rob = 2 + 9 + 1 = 12.
+     */
+	int rob(vector<int>& nums) {
+		int i=0, e=0;
+		for(int k=0; k<nums.size(); k++){
+			int tmp = i;
+			i = nums[k]+e;
+			e = Math.max(tmp, e);
+		}
+
+		return Math.max(i,e);
+	}
+
 	/**
 	 * 221. Maximal Square
 Given a 2D binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
