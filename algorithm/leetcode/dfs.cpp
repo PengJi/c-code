e558003d19413bb3cea5eda0af11c467a39605a1
pengji <jipeng92@gmail.com>
2018-06-07 19:34:47 +0800

    add 200

--- algorithm/leetcode/dfs.cpp
+++ algorithm/leetcode/dfs.cpp
@@ -613,4 +613,58 @@ https://leetcode.com/problems/palindrome-partitioning/discuss/41964/Clean-C++-ba
 
     	return true;
     }
+
+    /**
+     * 200. Number of Islands
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands 
+horizontally or vertically. You may assume all four edges of the grid are all 
+surrounded by water.
+
+Example 1:
+Input:
+11110
+11010
+11000
+00000
+Output: 1
+
+Example 2:
+Input:
+11000
+11000
+00100
+00011
+Output: 3
+
+problem:
+https://leetcode.com/problems/number-of-islands/description/
+     */
+    int m,n;
+	int numIslands(vector<vector<char>>& grid) {
+		int count = 0;
+		n = grid.size();
+		if(n == 0) return 0;
+		m = grid[0].size();
+
+		for(int i=0; i<n; i++){
+			for(int j=0; j<m; j++){
+				if(grid[i][j] == '1'){
+					dfs(grid, i, j);
+					++count;
+				}
+			}
+		}
+
+		return count;
+	}
+	void dfs(vector<vector<char>> &grid, int i, int j){
+		if(i<0 || j<0 || i>=n || j>=m || grid[i][j] != '1') return;
+
+		grid[i][j] = '0';
+		dfs(grid, i+1, j);
+		dfs(grid, i-1, j);
+		dfs(grid, i, j+1);
+		dfs(grid, i, j-1);
+	}
 };
