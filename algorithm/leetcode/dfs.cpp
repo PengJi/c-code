b8b7ca97c392a12c2b39e5920fbe7abbda634f63
pengji <jipeng92@gmail.com>
2018-06-11 22:21:36 +0800

    add 207

--- algorithm/leetcode/dfs.cpp
+++ algorithm/leetcode/dfs.cpp
@@ -667,4 +667,90 @@ https://leetcode.com/problems/number-of-islands/description/
 		dfs(grid, i, j+1);
 		dfs(grid, i, j-1);
 	}
+
+	/**
+	 * 207. Course Schedule
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have 
+to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, 
+is it possible for you to finish all courses?
+
+Example 1:
+Input: 2, [[1,0]] 
+Output: true
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+Input: 2, [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+             To take course 1 you should have finished course 0, and to take course 0 you should
+             also have finished course 1. So it is impossible.
+
+Note:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. 
+Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+	 */
+	//BFS
+    bool canFinish(int numCourses, vector<pair<int,int>> &prerequisites){
+    	vector<unordered_set<int>> graph = make_graph(numCourses, prerequisites);
+    	vector<int> degrees = compute_indegree(graph);
+    	for(int i=0; i<numCourses; i++){
+    		int j=0;
+    		for(; j<numCourses; j++)
+    			if(!degrees[j]) break;
+    		if(j == numCourses) return false;
+    		degrees[j] = -1;
+    		for(int neigh : graph[j])
+    			degrees[neigh]--;
+    	}
+
+    	return true;
+    }
+    vector<unordered_set<int>> make_graph(int numCourses, vector<pair<int, int>> &prerequisites){
+    	vector<unordered_set<int>> graph(numCourses);
+    	for(auto pre : prerequisites)
+    		graph[pre.second].insert(pre.first);
+    	return graph;
+    }
+    vector<int> compute_indegree(vector<unordered_set<int>>& graph){
+    	vector<int> degrees(graph.size(), 0);
+    	for(auto neighbors: graph)
+    		for(int neigh : neighbors)
+    			degrees[neigh]++;
+
+    	return degrees;
+    }
+	//DFS
+	vector<unordered_set<int>> make_graph(int numCourses, vector<pair<int, int>>& prerequisites){
+		vector<unordered_set<int>> graph(numCourses);
+		for(auto pre : prerequisites)
+			graph[pre.second].insert(pre.first);
+
+		return graph;
+	}
+	bool dfs_cycle(vector<unordered_set<int>>& graph, int node, vector<bool>& onpath, vector<bool>& visited){
+		if(visited[node]) return false;
+		onpath[node] = visited[node] = true;
+		for(int neigh : graph[node])
+			if(onpath[neigh] || dfs_cycle(graph, neigh, onpath, visited))
+				return true;
+
+		return onpath[node] = false;
+	}
+	bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites){
+		vector<unordered_set<int>> graph = make_graph(numCourses, prerequisites);
+		vector<bool> onpath(numCourses, false), visited(numCourses, false);
+		for(int i=0; i<numCourses; i++){
+			if(!visited[i] && dfs_cycle(graph, i, onpath, visited))
+				return false;
+		}
+
+		return true;
+	}
 };
