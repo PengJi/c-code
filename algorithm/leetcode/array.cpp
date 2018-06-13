d4834157945b6d33518854ee2756b3bab6e93422
pengji <jipeng92@gmail.com>
2018-06-13 20:55:14 +0800

    add 215

--- algorithm/leetcode/array.cpp
+++ algorithm/leetcode/array.cpp
@@ -1513,6 +1513,75 @@ https://leetcode.com/problems/majority-element/discuss/51612/6-Suggested-Solutio
         return major;
     }
 
+    /**
+     * 215. Kth Largest Element in an Array
+Find the kth largest element in an unsorted array. 
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+
+Example 2:
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+problem:
+https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+solution:
+https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60309/4-C++-Solutions-using-Partition-Max-Heap-priority_queue-and-multiset-respectively
+     */
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        return nums[k-1];
+    }
+    //quicksort
+    int partition(vector<int>& nums, int left, int right){
+        int pivot = nums[left];
+        int l = left+1, r = right;
+        while(l <= r){
+            if(nums[l] < pivot && nums[r] > pivot)
+                swap(nums[l++], nums[r--]);
+            if(nums[l] >= pivot) l++;
+            if(nums[r] <= pivot) r--;
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+    int findKthLargest(vector<int> &nums, int k){
+        int left = 0, right = nums.size()-1;
+        while(true){
+            int pos = partition(nums, left, right);
+            if(pos == k-1) return nums[pos];
+            if(pos > k-1) right = pos-1;
+            else left = pos+1;
+        }
+    }
+    //use priority_queue
+    int findKthLargest(vector<int>& nums, int k){
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for(int i=0; i<k-1; i++)
+            pq.pop();
+
+        return pq.top();
+    }
+    //use multiset
+    int findKthLargest(vector<int>& nums, int k){
+        multiset<int> mset;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            mset.insert(nums[i]);
+            if(mset.size() > k)
+                mset.erase(mset.begin());
+        }
+
+        return *mset.begin();
+    }
+
 	/*
 	 * 287. Find the Duplicate Number
 Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), 
