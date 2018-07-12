ac7d47f62082111b9ac8963399389eaf3b495d13
pengji <jipengpro@gmail.com>
2018-07-12 12:20:56 +0800

    modify list-test.cpp

--- algorithm/leetcode/list-test.cpp
+++ algorithm/leetcode/list-test.cpp
@@ -93,7 +93,27 @@ void RemoveNode(ListNode** pHead, int value){
 }
 
 int main(){
-    printf("test\n");
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+
+    PrintListNode(pNode4);
+    PrintList(pNode1);
+
+    AddToTail(&pNode1, 6);
+    PrintList(pNode1);
+
+    RemoveNode(&pNode1, 3);
+    PrintList(pNode1);
+
+    DestroyList(pNode1);
 
     return 0;
 }
