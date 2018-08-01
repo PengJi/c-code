71cd26df13304773cc4e0b3a2d9d5b472c396372
pengji <jipengpro@gmail.com>
2018-08-01 18:01:55 +0800

    modify files

--- algorithm/offer/2-3/03_02-DuplicationInArrayNoEdit.cpp
+++ algorithm/offer/2-3/03_02-DuplicationInArrayNoEdit.cpp
@@ -1,10 +1,10 @@
+#include <iostream>
+
 // 面试题3（二）：不修改数组找出重复的数字
 // 题目：在一个长度为n+1的数组里的所有数字都在1到n的范围内，所以数组中至
 // 少有一个数字是重复的。请找出数组中任意一个重复的数字，但不能修改输入的
 // 数组。例如，如果输入长度为8的数组{2, 3, 5, 4, 3, 2, 6, 7}，那么对应的
 // 输出是重复的数字2或者3。
-
-#include <iostream>
 
 int countRange(const int* numbers, int length, int start, int end);
 
